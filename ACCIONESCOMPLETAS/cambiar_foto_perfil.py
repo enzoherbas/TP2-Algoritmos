5e743a2b07c8480883896c98e2d8bb46338535a9
tcorsico <tcorsico@fi.uba.ar>
2021-02-15 18:59:38 -0300

    Cambiar foto de perfil.

---
+++ ACCIONESCOMPLETAS/cambiar_foto_perfil.py
@@ -0,0 +1,83 @@
+import facebook
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+from pyfacebook import Api
+import json
+
+api = Api(
+        app_id = "692001264799472",
+        app_secret = "60b272a45b500fef45f3c930d5d6d8df",
+        long_term_token = "EAAJ1XxmSjvABAIVSXdbeDkCVQuewmUMOs8ZClysBW8NWZBMx3zGR2wN3EWZBUwjlUfSh2NF7jDztlXSALCal8VYjGZAd69wZA0xd5XUBJpB6YY3bcZC1SZBV7juZCpnBHHdc8X6ZBN1O6CjAZBt9nWPZC4BY1v0KJfRGkhpRvXjiaZA1oPS90vt6HJcRIynEvxDadJsZD",
+        )
+long_term_token = "EAAJ1XxmSjvABAIVSXdbeDkCVQuewmUMOs8ZClysBW8NWZBMx3zGR2wN3EWZBUwjlUfSh2NF7jDztlXSALCal8VYjGZAd69wZA0xd5XUBJpB6YY3bcZC1SZBV7juZCpnBHHdc8X6ZBN1O6CjAZBt9nWPZC4BY1v0KJfRGkhpRvXjiaZA1oPS90vt6HJcRIynEvxDadJsZD",
+api_2 = facebook.GraphAPI(long_term_token)
+Tk().withdraw()
+
+def foto_archivo(page_id):
+        try:
+                filename = askopenfilename() 
+                api_2.put_photo(image = open(r"{0}".format(filename),"rb"), album_path=f"{page_id}/picture")
+        except:
+                print("no pasa nada pa")                
+
+def foto_url(page_id):
+        url = input("Ingrese url de la imagen: ")
+        post_args = {"picture":url,"access_token":api._access_token}
+        peticion = api._request(path=f"v9.0/{page_id}/picture",method="POST",post_args = post_args)
+        # data = api._parse_response(peticion)
+        # print(data)
+        print("se ha cambiado al foto de perfil")
+
+def listar_fotos_publicadas(page_id):
+        cont = 1
+        peticion = api._request(path = f"v9.0/{page_id}/photos?type=uploaded", method = "GET")
+        datos = api._parse_response(peticion)
+        lista_ids = [] 
+        for ids in datos["data"]:
+                lista_ids.append(ids["id"])
+        for ids in lista_ids:
+                peticion_2 = api._request(path = f"v9.0/{page_id}_{ids}?fields=permalink_url", method = "GET")
+                datos_2 = api._parse_response(peticion_2)
+                print(cont, datos_2["permalink_url"])
+                cont += 1
+        opcion = validacion_en_rango(1,cont)
+        foto_seleccionada = lista_ids[opcion-1]
+        return foto_seleccionada
+        
+def foto_ya_publicada(page_id):
+        reuse = True
+        foto = listar_fotos_publicadas(page_id)
+        access_token = api._access_token
+        post_args = {"photo":foto, "access_token":access_token, "reuse":reuse}
+        peticion = api._request(path = f"v9.0/{page_id}/picture", method = "POST", post_args = post_args)
+        # data = api._parse_response(peticion)
+        # return data
+        print("Se ha cambiado la foto de perfil")
+
+def validacion_en_rango(rango_min, rango_max):
+    opcion = input("Opcion: ")
+    while not opcion.isnumeric() or int(opcion) not in range(rango_min, rango_max):
+        opcion = input("La opcion ingresada no es valida. Por favor, vuelva a intentar: ")
+    return int(opcion)
+
+def eleccion(page_id):
+        print("""Elija una opcion:
+        1. Subir una nueva foto de perfil
+        2. Seleccionar una ya publicada """)
+        opcion = validacion_en_rango(1,3)
+        if opcion == 1:
+                print("""Elija una opcion:
+        1. Seleccionar archivo
+        2. Mediante URL """)
+                opcion_2 = validacion_en_rango(1,3)
+                if opcion_2 == 1:
+                        foto_archivo(page_id)
+                if opcion_2 == 2:
+                        foto_url
+        if opcion == 2:
+                foto_ya_publicada(page_id)
+                        
+def main():
+        page_id = "341526406956810"
+        eleccion(page_id)
+main()
