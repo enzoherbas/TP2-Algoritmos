12f024d9dd089c9466d1d9820aa6137bc2aebd58
tcorsico <tcorsico@fi.uba.ar>
2021-02-17 17:43:34 -0300

    validacion

--- ACCIONES/post_images.py
+++ ACCIONES/post_images.py
@@ -1,16 +1,29 @@
 import facebook
-from tkinter import Tk
-from tkinter.filedialog import askopenfilename
+import facebook
+import tkinter as tk
+from tkinter import filedialog
+
+window = tk.Tk()
+window.wm_attributes('-topmost', 1)
+window.withdraw()
 
 long_term_token = "EAAJ1XxmSjvABAIVSXdbeDkCVQuewmUMOs8ZClysBW8NWZBMx3zGR2wN3EWZBUwjlUfSh2NF7jDztlXSALCal8VYjGZAd69wZA0xd5XUBJpB6YY3bcZC1SZBV7juZCpnBHHdc8X6ZBN1O6CjAZBt9nWPZC4BY1v0KJfRGkhpRvXjiaZA1oPS90vt6HJcRIynEvxDadJsZD",
 
 api = facebook.GraphAPI(long_term_token)
 
-Tk().withdraw()
-filename = askopenfilename() 
 
 def post_image():
-    api.put_photo(image = open(r"{0}".format(filename),"rb"), message = input("Ingrese descripcion de la foto: "))
-    
+        archivos_soportados = ["peg","bmp","png","gif","iff"]
+        ingreso_correcto = False
+        while not ingreso_correcto:
+                filename = filedialog.askopenfilename()
+                if filename[-3::] in archivos_soportados:
+                        ingreso_correcto = True
+                else:
+                        print("El archivo elegido es incorrecto, vuelva a elegir.")
+        api.put_photo(image = open(r"{0}".format(filename),"rb"), message = input("Ingrese descripcion de la foto: "))
+        print("Se ha subido el posteo")
+
+
 if __name__ == "__main__":
     post_image()
