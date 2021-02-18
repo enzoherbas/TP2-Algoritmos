3c8f205d152d4e27547a76510c228fcb71ff7051
“enzoherbas” <jeherbas@fi.uba.ar>
2021-02-17 22:31:47 -0300

    Cambios agregados del Funcionamiento con Bot

--- PrototipoBot.py
+++ PrototipoBot.py
@@ -2,16 +2,18 @@ import json
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from pyfacebook import Api
+from datetime import date
+from datetime import datetime
 
 DATOS = Api(app_id = "692001264799472",app_secret = "60b272a45b500fef45f3c930d5d6d8df",long_term_token = "EAAJ1XxmSjvABAIVSXdbeDkCVQuewmUMOs8ZClysBW8NWZBMx3zGR2wN3EWZBUwjlUfSh2NF7jDztlXSALCal8VYjGZAd69wZA0xd5XUBJpB6YY3bcZC1SZBV7juZCpnBHHdc8X6ZBN1O6CjAZBt9nWPZC4BY1v0KJfRGkhpRvXjiaZA1oPS90vt6HJcRIynEvxDadJsZD",)
 ID_PAGINA = "341526406956810"
-CRUX = ChatBot("prototipo_bot",logic_adapters=[
+CRUX = ChatBot("bot_10",logic_adapters=[
         {
             'import_path': 'chatterbot.logic.BestMatch',
             'default_response': 'Disculpa, no logro entenderte. Intenta escribirlo de otra manera',
-            'maximum_similarity_threshold': 0.80
-        }
-    ])
+            'maximum_similarity_threshold': 0.90
+        }])
+LOG = open("log.txt","w")
 
 def entrenamiento_bot():
     '''
@@ -25,18 +27,17 @@ def entrenamiento_bot():
     entrenamiento = ListTrainer(CRUX)
     texto = open("trainer.txt")
     respuestas = []
-    contador = 0
     for linea in texto:
         linea = linea.rstrip("\n").split(",")
-        #entrenamiento.train(linea)
-        if linea[-1] not in respuestas and contador <= 8:
+        print(linea)
+        entrenamiento.train(linea)
+        if linea[-1] not in respuestas and len(respuestas) <= 8:
             respuestas.append(linea[-1])
-            contador += 1
     print(respuestas)
     texto.close()
     return respuestas
 
-def opciones_bot():
+def opciones_bot(usuario):
     '''
     PRE:
     Se ingresara una vez sea solicitado este menu, es un printeo de las opciones las cuales se pueden realizar, para que
@@ -65,10 +66,8 @@ def like_posteo(post_id):
     '''
     post_args = {"access_token":DATOS._access_token}
     peticion = DATOS._request(path="v9.0/{0}/likes".format(post_id),method="POST",post_args = post_args)
-    print("Se logro likear con exito el post!")
-    return True
 
-def ver_posteos():
+def ver_posteos(usuario,respuestas):
     '''
     PRE:
     Se ingresa mediante el menu de dialogo del BOT, desde aca se veran los posts y realizaran las modificaciones a los mismos.
@@ -78,6 +77,7 @@ def ver_posteos():
     Se envia los datos recolectados como el ID de cada publicacion, con el fin de poder realizar la modificacion. En este caso
     las modicaciones se enviaran a LIKE_POSTEO o ACTUALIZAR_POSTEO
     '''
+    
     posts_id = []
     informacion_posts = DATOS.get_page_posts(page_id = ID_PAGINA,fields= "story,message,permalink_url",return_json = True,count=None)
     contador_posts = 1
@@ -85,29 +85,54 @@ def ver_posteos():
         if ("story") not in informacion_post:        
             try:
                 print("N°{0} Post.\n-{1}".format(contador_posts,informacion_post["message"]))
-                contador_posts += 1 
+                post_id = (contador_posts,informacion_post["id"])
+                posts_id.append(post_id)
+                contador_posts += 1     
             except:
                 print("N°{0} Post.\n-Imagen\n-Url: {1}".format(contador_posts,informacion_post["permalink_url"]))
+                post_id = (contador_posts,informacion_post["id"])
+                posts_id.append(post_id)
                 contador_posts += 1 
-            post_id = (contador_posts,informacion_post["id"])
-            posts_id.append(post_id)
     dicc_posts = dict(posts_id)
     finalizar_modificaciones = False
     while finalizar_modificaciones == False:        
-        eleccion_modificacion = input("Deseas realizar una modificacion?\nIngrese SI o NO")
-        if eleccion_modificacion == "SI":
-                numero_post_elegido = int(input("Ingrese el numero de post a modificar: "))
-                selector_modificacion = input("Ingrese ME GUSTA o CAMBIAR TEXTO para realizar la accion: ")
-                if selector_modificacion == "ME GUSTA":
-                    like_posteo(dicc_posts[numero_post_elegido])   
+        acciones_bot("seccion1")
+        respuesta_usuario = input("{0}:".format(usuario)).upper()
+        registro_log(respuesta_usuario,usuario)
+        print(respuesta_usuario)
+        if respuesta_usuario == "SI":
+            eleccion_modificacion = True
+            while eleccion_modificacion == True:
+                    acciones_bot("seccion2")
+                    numero_post_elegido = acciones_usuario(usuario)
+                    try:
+                        if numero_post_elegido.isnumeric and int(numero_post_elegido) < contador_posts:
+                            eleccion_modificacion = False
+                        else:
+                            acciones_bot("seccion4")
+                    except:
+                        acciones_bot("seccion4")               
+            finalizar_accion = False
+            while finalizar_accion == False:
+                acciones_bot("seccion5")
+                selector_modificacion = mensajes(usuario)
+                if str(selector_modificacion) == respuestas[2]:
+                    like_posteo(dicc_posts[int(numero_post_elegido)])
+                    acciones_bot("seccion6")
+                    finalizar_accion = True 
+                elif str(selector_modificacion) == respuestas[3]:
+                    actualizar_post(dicc_posts[int(numero_post_elegido)])
+                    acciones_bot("seccion6")
+                    finalizar_accion = True                   
                 else:
-                    actualizar_post(dicc_posts[numero_post_elegido])                
+                    acciones_bot("seccion4")
         else:
+            acciones_bot("seccion3")
             finalizar_modificaciones = True
             
     return True
 
-def subir_posteo():
+def subir_posteo(usuario):
     '''
     PRE:
     Se ingresa mediante la conversacion con el bot, desde aca podremos realizar una creacion de post. Que sea unicamente 
@@ -130,9 +155,8 @@ def actualizar_post(post_id):
     modificacion_mensaje = input("Ingrese el nuevo texto del post: ")
     post_args = {"access_token":DATOS._access_token,"message":modificacion_mensaje}
     peticion = DATOS._request(path="v9.0/{0}".format(post_id,modificacion_mensaje),method="POST",post_args = post_args)
-    print("El posteo se actualizo con exito!")
 
-def cantidad_seguidores():
+def cantidad_seguidores(usuario):
     '''
     Muestra la cantidad de seguidores,likes y nombre que tiene la pagina
     '''
@@ -147,7 +171,7 @@ def cantidad_seguidores():
           '''.format(data["followers_count"],data["fan_count"],data["name"]))
     return True
 
-def actualizar_datos():
+def actualizar_datos(usuario):
     '''
     Actualiza los datos de la pagina
     '''
@@ -181,14 +205,14 @@ def actualizar_datos():
     print("Se modificaron los datos con exito!")
     return True
 
-def subir_foto():
+def subir_foto(usuario):
     '''
     Hay que ver si se junta junto con la funcion SUBIR_POSTEO
     '''
     print("Subir Foto")
     return True
 
-def finalizar():
+def finalizar(usuario):
     '''
     Opcion ingresada mediante el chat de bot
     POST:
@@ -200,22 +224,10 @@ def conversacion(usuario,respuestas_clave):
     '''
     PRE:
     Toma el nombre del usuario y las respuestas previamente guardadas del entrenamiento de crux en ENTRENAMIENTO_BOT.
-    Se generara una charla con el nombre del usuario y del bot. Desde aca se redigiran a las distintas opciones
+    Se generara una charla con el nombre del usuario y del bot.
     POST:
-    Se guardara una charla en el LOG (Aun no creado) donde estara NOMBRE, HORA Y TEXTO EMITIDO
-    '''
-    continuar = True
-    while continuar == True:
-        peticion = input("{0}:".format(usuario))
-        respuesta_bot = CRUX.get_response(peticion)
-        print("Crux:{0}".format(respuesta_bot))
-        if str(respuesta_bot) in respuestas_clave:
-            indice_respuesta = respuestas_clave.index(str(respuesta_bot)) 
-            continuar = selector_opciones(indice_respuesta)
-            
-def selector_opciones(respuesta):
-    '''
-    Diccionario de funciones a ingresar mediante a la respuesta que dara el BOT
+    Se redirecciona a las distintas funciones y a medida que ocurre se generan los cambios en el LOG. La charla
+    continua hasta que el usuario decida Finalizarla mediante la conversacion con el bot
     '''
     opciones = {0:opciones_bot,
                 1:ver_posteos,                
@@ -225,13 +237,47 @@ def selector_opciones(respuesta):
                 7:actualizar_datos,
                 8:finalizar
                 }
-    accion = opciones[respuesta]()
-    return accion
+    acciones_bot("saludo inicial")
+    continuar = True
+    while continuar == True:        
+        respuesta_bot = mensajes(usuario)
+        if str(respuesta_bot) in respuestas_clave:
+            indice_respuesta = respuestas_clave.index(str(respuesta_bot))
+            if indice_respuesta is 1:
+                continuar = opciones[indice_respuesta](usuario,respuestas_clave)
+            else:
+                continuar = opciones[indice_respuesta](usuario)
 
+def registro_log (dialogo,usuario):
+    hora_actual = datetime.now()
+    dialogo_log = str("{} {}:{}:{} {} : {}"
+                .format(date.today(),hora_actual.hour,hora_actual.minute,hora_actual.second,usuario,dialogo))
+    LOG.write(dialogo_log+"\n")
+
+def mensajes(usuario):
+    peticion = input("{0}:".format(usuario))
+    registro_log(peticion,usuario)
+    peticion = peticion.lower()
+    respuesta_bot = CRUX.get_response(peticion)
+    registro_log(respuesta_bot,"Crux")
+    print("Crux:{0}".format(respuesta_bot))
+    return respuesta_bot
+
+def acciones_bot(codigo_accion):
+    lectura_accion = CRUX.get_response(codigo_accion)
+    print("Crux: {0}".format(lectura_accion))
+    registro_log(lectura_accion,"Crux")
+
+def acciones_usuario(usuario):
+    accion_usuario = input("{0}: ".format(usuario))
+    registro_log(accion_usuario,usuario)
+    return accion_usuario
+    
 def main():
     entrenamiento = entrenamiento_bot()
     usuario = input("Ingrese su nombre: ")
     charla = conversacion(usuario,entrenamiento)
+    LOG.close()
     print("FIN")
     
 main()
